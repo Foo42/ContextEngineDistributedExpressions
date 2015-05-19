546694249219457a6bcf1ce71bcfc2edfa386c12
Julian Haeger <julianhaeger@gmail.com>
2015-05-19 08:59:36 +0100

    Provider contracts now have a replaceable stopWatch function to support providers delaying the emission of the notWatching even until they have performed any required actions

--- test/unit/providerContract.js
+++ test/unit/providerContract.js
@@ -69,5 +69,70 @@ describe('providerContract', function () {
                 contract.handleMessage('watch')
             });
         });
+
+    });
+    describe('stopping watching', function () {
+        var contract;
+        var eventAndPublishSeen;
+        var contract;
+        var onEventRecieved;
+        var onPublishCalled;
+        var publishCalled;
+        var eventRecieved;
+
+        beforeEach(function (done) {
+            publishCalled = new Promise(function (resolve) {
+                onPublishCalled = resolve;
+            });
+            eventRecieved = new Promise(function (resolve) {
+                onEventRecieved = resolve;
+            });
+            eventAndPublishSeen = Promise.all([
+                publishCalled,
+                eventRecieved
+            ]);
+
+            var fakeDistexExchange = {
+                publish: function (message) {
+                    if (/notWatching$/.test(message)) {
+                        onPublishCalled();
+                    }
+                }
+            };
+            contract = createContract('123', 'some expression', fakeDistexExchange);
+            contract.on('notWatching', function () {
+                onEventRecieved();
+            })
+            contract.on('watching', done);
+            contract.handleMessage('watch');
+        });
+
+        describe('without custom stopWatching function', function () {
+            it('should emit notWatching event and distex message straight away', function (done) {
+                eventAndPublishSeen.then(function () {
+                    done();
+                });
+
+                contract.handleMessage('stopWatching')
+            });
+        });
+        describe('with a custom stopWatching function', function () {
+            it('should not emit notWatching event or distex message until promise returned by stopWatching is fulfilled', function (done) {
+                var tooSoon = true;
+                Promise.any([eventRecieved, publishCalled]).then(function () {
+                    tooSoon ? done(new Error('event or message fired before custom stopWatching function has completed')) : done();
+                });
+
+                contract.stopWatching = function () {
+                    return new Promise(function (resolve) {
+                        setTimeout(resolve, 200);
+                    }).then(function () {
+                        tooSoon = false;
+                    });
+                };
+
+                contract.handleMessage('stopWatching');
+            });
+        });
     });
 });
